76b8caf76885669afdbf2a15bf19dbb079c87605
Nguyen Tri Thanh <nguyentrithanh1311@gmail.com>
2025-07-28 23:20:53 +0700

    Update code 28/7/2025.

--- api/src/shared/interfaces/filter-data.interface.ts
+++ api/src/shared/interfaces/filter-data.interface.ts
@@ -1,16 +1,10 @@
 interface FilterData<T> {
   page: number;
-
   limit: number;
-
   search: string;
-
   searchColumns: (keyof T)[];
-
   filters: Partial<T> & Record<string, any>;
-
   orderBy: keyof T;
-
   orderDir: "ASC" | "DESC" | null;
 }
 
--- api/src/shared/interfaces/paginate-data.interface.ts
+++ api/src/shared/interfaces/paginate-data.interface.ts
@@ -1,16 +1,10 @@
 interface PaginateData<T> {
   data: T[];
-
   page: number;
-
   limit: number;
-
   totalRecords: number;
-
   totalPages: number;
-
   hasPrev: boolean;
-
   hasNext: boolean;
 }
 
